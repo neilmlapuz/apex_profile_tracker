51bea06750276dcd27c740df8665a05578e7b00e
root <root@DESKTOP-NILVTMJ.localdomain>
2019-08-24 16:46:51 +0100

    added http post request to server

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express'),
     app = express(),
-    dotenv = require('dotenv')
+    dotenv = require('dotenv'),
+    path = require('path'),
+    bodyParser = require('body-parser')
+
 dotenv.config({ path: './config.env' })
 
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json()) // for parsing application/json
 
-app.use(express.static(__dirname + '/public'));
+
 app.use('/', require('./routes/index'))
-app.use('/api/v1/profile', require('./routes/profile'))
+app.post('/handler', function (req, res) {
+    console.log(req.body.gamertag)
+})
+
+// app.use('/api/v1/profile', require('./routes/profile'))
 
 
 
