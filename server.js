0953f083d91e0364f28f018ae553aae4d515eb80
root <root@DESKTOP-NILVTMJ.localdomain>
2019-08-25 13:55:23 +0100

    removed index routes

--- server.js
+++ server.js
@@ -11,17 +11,22 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json()) // for parsing application/json
 
 
-app.use('/', require('./routes/index'))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+
+})
 
 
-app.post('/handler', function (req, res) {
-    console.log(">>>.SD")
+app.post('/', function (req, res) {
+    console.log(">>>.SD", req.body.platform)
     // res.send(`/api/v1/profile/${req.body.platform}/${req.body.gamertag}`)
-    // app.use(`/api/v1/profile/${req.body.platform}/${req.body.gamertag}`, require('./routes/profile'))
+    // res.send(req.body)
+    return res.redirect('/profile')
+    // app.use(profile/${req.body.platform}/${req.body.gamertag}`, require('./routes/profile'))
     // console.log(req.body.gamertag)
 })
 
-// app.use('/api/v1/profile', require('./routes/profile'))
+// app.use('/profile', require('./routes/profile'))
 
 
 
